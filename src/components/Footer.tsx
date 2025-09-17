3cf547cead5195901360ae55d3cf3cf2134234a5
d3gen <199314779+d3g3n1@users.noreply.github.com>
2025-09-17 12:52:55 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,16 +14,17 @@ export default function Footer(){
       }}
     >
       <div className='muted'>© BlueCat Club • On Base • Please play responsibly.</div>
-
       <div className='row' style={{gap:8}}>
-        {/* Replace '#' with your real links when ready */}
-        <a className='pill' aria-label='Open X' title='X / Twitter' href='#' target='_blank' rel='noreferrer' style={{background:'#0d1c31'}}>
+        <a className='pill' aria-label='Open X' title='X / Twitter'
+           href='https://x.com/bluecat_club' target='_blank' rel='noreferrer' style={{background:'#0d1c31'}}>
           <img src='/x.svg' style={{width:22,height:22}}/>
         </a>
-        <a className='pill' aria-label='Open Telegram' title='Telegram' href='#' target='_blank' rel='noreferrer' style={{background:'#0d1c31'}}>
+        <a className='pill' aria-label='Open Telegram' title='Telegram'
+           href='https://t.me/bluecatclub' target='_blank' rel='noreferrer' style={{background:'#0d1c31'}}>
           <img src='/telegram.svg' style={{width:22,height:22}}/>
         </a>
-        <a className='pill' aria-label='Open GitHub' title='GitHub' href='#' target='_blank' rel='noreferrer' style={{background:'#0d1c31'}}>
+        <a className='pill' aria-label='Open GitHub' title='GitHub'
+           href='https://github.com/d3g3n1/bluecat-club' target='_blank' rel='noreferrer' style={{background:'#0d1c31'}}>
           <img src='/github.svg' style={{width:22,height:22}}/>
         </a>
       </div>
